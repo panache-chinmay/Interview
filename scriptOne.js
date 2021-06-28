b21fcda43d897990e9aae726756146bc64cc8669
panache-chinmay <chinmaydeshpande010@gmail.com>
2021-06-28 15:43:15 +0530

    new2

---
+++ scriptOne.js
@@ -0,0 +1,21 @@
+
+
+
+
+
+
+
+
+let obj ={
+    title: 9,
+    context: 86,
+    steps_to_reproduce: 16,
+    expected_result: 32,
+    actual_result: 24,
+    user_impact: 106,
+    workaround: 51,
+    environment: 51,
+    version_info: 88,
+    occurrence: 54,
+    links: 119
+  }
